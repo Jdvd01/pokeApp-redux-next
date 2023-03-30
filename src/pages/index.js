7a2acfa93ad9de29cc37f3bdbe9082145b42083f
Jose Velasquez <velasquezdenorajose@gmail.com>
2023-03-30 18:43:33 -0400

    Landing component

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,13 +1,13 @@
-import Head from 'next/head'
 import { Inter } from 'next/font/google'
-import PokemonCards from '@/components/PokemonCards'
 import { Box } from '@chakra-ui/react'
+import Link from 'next/link'
 const inter = Inter({ subsets: ['latin'] })
 
 export default function Home() {
     return (
-        <Box bg={"#FFE4B5"} minHeight="100vh" py={8} w="100%">
-            <PokemonCards />
+        <Box bg={"#FFE4B5"} minHeight="100vh" p={8} w="100%">
+            Landing
+            <Link href="/pokemon">Pokemon list</Link>
         </Box>
     )
 }
